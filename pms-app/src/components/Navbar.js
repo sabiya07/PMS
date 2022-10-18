8a65bf32c86cb0159ddcf189607c921cd69381c4
sabiya07 <mohammedsabiya2@gmail.com>
2022-10-18 19:14:36 +0530

    sabiya pms commit

---
+++ pms-app/src/components/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar(){
+    return (
+        <div className="menu">
+            <a href="#">About Us</a>
+            <a href="#">Contact Us</a>
+            <a href="#">Admin</a>
+        </div>
+    )
+}
+export default Navbar;
